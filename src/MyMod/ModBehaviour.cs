6b4fb3cc8a38d0ae557a5b85f088c0496289e313
weitianhan <weith9452@gmail.com>
2026-02-22 11:07:07 +0800

    chore: bootstrap MyMod hello world

---
+++ src/MyMod/ModBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MyMod
+{
+    public class ModBehaviour : Duckov.Modding.ModBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("[MyMod] ModBehaviour.Start() loaded!");
+        }
+
+        private void Update()
+        {
+            // Keep Update lightweight; avoid spam logging.
+        }
+    }
+}
